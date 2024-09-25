02b6cccd40dfb9ab7363c37f089e7d64851b9913
huangzq <13402318457@163.com>
2024-09-26 01:00:33 +0800

    feat: update

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
+  publicPath: './',
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000', // 本地的接口
+        // target: 'http://10.11.0.10:9999/api', // 线上的接口
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }, // 重写路径，将 /api 移除
+      },
+    },
+  },
+};
